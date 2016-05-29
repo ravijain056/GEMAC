7ef9b5d2b751b242defba9e0ec4b8ed62f245e97
Ravi Jain <ravijain056@gmail.com>
2016-05-30 02:10:35 +0530

    Initialised sub-modules.

--- gemac/gemac.py
+++ gemac/gemac.py
@@ -20,5 +20,25 @@ def gemac(
         mdc, mdioIn, mdioOut, mdioTri):
     
     
+    transmitPauseFrame = Signal(bool(0))
+    txData2Engine  = Signal(intbv(0, min = 0, max = 255))
+    txData2Client  = Signal(intbv(0, min = 0, max = 255))
+    
+    clientInst = client(gtxClk, txData, txDataValid, txIFG_delay, tx_ack, tx_underrun,
+                        rxData, rxDataValid, rxGoodFrame, rxBadFrame,
+                        txData2Engine, rxData2Client)
+    
+    txEngineInst = txEngine(txData2Engine, transmitEN, txData2GMII)
+    
+    rxEngineInst = rxEngine(rxData2Client, rxData2Engine)
+    
+    flowControlInst = flowControl(pauseReq, pauseVal, transmitPauseFrame)
+    
+    gmiiInst = gmii(txData2GMII, rxData2Engine,
+                    gmiiTxd, gmiiTxEn, gmiiTxEr,
+                    gmiiRxClk, gmiiRxd, gmiiRxDv, gmiiRxEr)
+    
+    mdioInst = mdio(hostClk, hostOpcode, hostAddr, hostWriteData, hostReadData, hostMIIM_sel, hostReq, hostMIIM_rdy
+                    mdc, mdioIn, mdioOut, mdioTri)
     
     return instances()
