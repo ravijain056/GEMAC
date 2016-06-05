aebf0cad09efb45d82146aa260e8e7d60a49afde
Ravi Jain <ravijain056@gmail.com>
2016-06-05 11:59:56 +0530

    Updated modular base.

--- gemac/interfaces.py
+++ gemac/interfaces.py
@@ -1,22 +1,18 @@
-from myhdl import Signal, intbv
+from myhdl import Signal, intbv, ResetSignal
 
 
-class flowControlInterface:
-
+class FlowControlInterface:
     def __init__(self):
-
         self.pausereq = Signal(bool(0))  # Pause Request
         self.pauseval = Signal(intbv(0)[16:])  # Pause Value
 
 
-class managementInterface():
-
+class HostManagementInterface():
     def __init__(self):
-
         # Client Management Interface
-        self.hostclk = Signal(bool(0))  # Host Clock
+        self.clk = Signal(bool(0))  # Host Clock
         self.opcode = Signal(intbv(0)[2:])  # host Opcode
-        self.regaddress = Signal(intbv(0)[10:])  # Configuration Register Address
+        self.regaddress = Signal(intbv(0)[10:])  # Configuration Register Addr
         self.wrdata = Signal(intbv(0)[32:])  # Write Data
         self.rddata = Signal(intbv(0)[32:])  # Read Data
         self.miimsel = Signal(bool(0))  # MIIM select
@@ -24,10 +20,8 @@ class managementInterface():
         self.miimrdy = Signal(bool(0))  # hostMIIM_ready
 
 
-class mdioInterface:
-
+class MDIOInterface:
     def __init__(self):
-
         # MDIO PHY Interface
         self.mdc = Signal(bool(0))  # Management Clock derived from Host Clock
         self.mdioIn = Signal(bool(0))
@@ -35,15 +29,12 @@ class mdioInterface:
         self.mdioTri = Signal(bool(0))
 
 
-class phyInterface:
-
+class PHYInterface:
     def __init__(self):
-
         # GMII PHY Transmitter Interface
         self.txd = Signal(intbv(0)[8:])  # Transmit Data
         self.txen = Signal(bool(0))  # Transmit Enable
         self.txer = Signal(bool(0))  # Transmit Error
-
         # GMII PHY Receiver Interface
         self.rxclk = Signal(bool(0))  # Receive Clock
         self.rxd = Signal(intbv(0)[8:])  # Receive Data
@@ -51,24 +42,51 @@ class phyInterface:
         self.rxer = Signal(bool(0))  # Receive Error
 
 
-class rxClientInterface:
-
+class RxFIFOClientInterface:
     def __init__(self):
-
+        self.rxclk = Signal(bool(0))
+        self.reset = ResetSignal(0, active=0, async=False)
+        self.clkenable = Signal(bool(0))
         self.rxd = Signal(intbv(0)[8:])  # Receive Data
         self.rxdv = Signal(bool(0))  # Receive Data Valid
         self.rxgood = Signal(bool(0))  # Receive Good Frame
         self.rxbad = Signal(bool(0))  # Receive Bad Frame
+        self.overflow = Signal(bool(0))
 
 
-class txClientInterface:
-
+class RxLocalLinkFIFOInterface:
     def __init__(self):
+        self.rxclk = Signal(bool(0))
+        self.reset = ResetSignal(0, active=0, async=False)
+        self.rxd = Signal(intbv(0)[8:])
+        self.sof = Signal(bool(0))
+        self.eof = Signal(bool(0))
+        self.src_rdy = Signal(bool(0))  # Source Ready
+        self.dst_rdy = Signal(bool(0))  # Destination Ready
+        self.fifostatus = Signal(bool(0))
 
+
+class TxFIFOClientInterface:
+    def __init__(self):
         # Client Transmitter Interface
         self.gtxclk = Signal(bool(0))
         self.txd = Signal(intbv(0)[8:])  # Transmit Data
         self.txdv = Signal(bool(0))  # Transmit Data Valid
         self.txifgdelay = Signal(intbv(0)[16:])  # Transmit InterFrameGap Delay
-        self.txack = Signal(bool(0))  # Transmit Acknowledge 
+        self.txack = Signal(bool(0))  # Transmit Acknowledge
         self.txunderrun = Signal(bool(0))  # Transmit Underrun
+        self.txcollision = Signal(bool(0))
+        self.txretransmit = Signal(bool(0))
+
+
+class TxLocalLinkFIFOInterface:
+    def __init__(self):
+        self.txclk = Signal(bool(0))
+        self.reset = ResetSignal(0, active=0, async=False)
+        self.txd = Signal(intbv(0)[8:])
+        self.sof = Signal(bool(0))
+        self.eof = Signal(bool(0))
+        self.src_rdy = Signal(bool(0))  # Source Ready
+        self.dst_rdy = Signal(bool(0))  # Destination Ready
+        self.fifostatus = Signal(intbv(0)[4:])
+        self.overflow = Signal(bool(0))
--- tests/test_Gemac.py
+++ tests/test_Gemac.py
@@ -1,22 +1,24 @@
 from gemac.gemac import gemac
-from gemac.interfaces import phyInterface, flowControlInterface,\
-    managementInterface, txClientInterface, rxClientInterface, mdioInterface
+from gemac.interfaces import PHYInterface, FlowControlInterface,\
+    HostManagementInterface, TxFIFOClientInterface, RxFIFOClientInterface,\
+    MDIOInterface
 from myhdl import ResetSignal
+
 
 class TestGemacCore:
 
     def setup_method(self, method):
 
-        txclient_interface = txClientInterface()
-        rxclient_interface = rxClientInterface()
-        phy_interface = phyInterface()
-        flow_interface = flowControlInterface()
-        management_interface = managementInterface()
-        mdio_interface = mdioInterface()
+        txclient_interface = TxFIFOClientInterface()
+        rxclient_interface = RxFIFOClientInterface()
+        phy_interface = PHYInterface()
+        flow_interface = FlowControlInterface()
+        hostmanagement_interface = HostManagementInterface()
+        mdio_interface = MDIOInterface()
         reset = ResetSignal(0, active=0, async=True)
         gemacInst = gemac(txclient_interface, rxclient_interface,
-                          phy_interface, flow_interface, management_interface,
-                          mdio_interface, reset)
+                          phy_interface, flow_interface,
+                          hostmanagement_interface, mdio_interface, reset)
         print("Testing %s" % gemacInst)
 
     def test_nothing(self):
