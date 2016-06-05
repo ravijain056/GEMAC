1b4cd317858e3e57fa30166addcd9d1e6bcc2204
Ravi Jain <u13ce088@ced.svnit.ac.in>
2016-06-06 04:59:45 +0530

    Integrated pytest with setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
+import sys
+from setuptools.command.test import test as TestCommand
 from setuptools import setup
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(
     name='gemac',
+    version = '0.1',
     author='Ravi Jain',
     author_email='ravijain056@gmail.com',
     url='http://github.com/ravijain056/GEMAC',
     packages=['gemac'],
     description='GEMAC implementation and tools',
-    install_requires = ['myhdl >= 1.0.dev0'],
+    long_description=open('README.md').read() + '\n\n',
+    install_requires=['myhdl >= 1.0.dev0'],
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
