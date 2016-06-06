974d809711e924f840e75d9843ba83885a9211b1
Ravi Jain <ravijain056@gmail.com>
2016-06-07 04:10:34 +0530

    Added Test for mdc clk gen.

--- tests/test_mangement.py
+++ tests/test_mangement.py
@@ -1,25 +1,17 @@
 from gemac.interfaces import HostManagementInterface, MDIOInterface
 from gemac.management import management
-from myhdl import instance, delay, block, intbv
+from myhdl import instance, delay, block, intbv, now
 
 
 def test_readwriteconfig():
 
     @block
-    def writeconfig_operation():
+    def test():
 
         hostmanagement_interface = HostManagementInterface()
         mdio_interface = MDIOInterface()
         managementInst = management(hostmanagement_interface, mdio_interface)
         print("Testing Read/Write Configuration Register%s" % managementInst)
-
-        @instance
-        def write():
-            yield clkWait(count=10)
-            yield hostmanagement_interface.writeconfig(0x340, 0x12345678)
-            yield clkWait(count=4)
-            yield hostmanagement_interface.readconfig(0x340)
-            assert hostmanagement_interface.rddata[32:] == intbv(0x12345678)
 
         @instance
         def hostclkdriver():
@@ -28,13 +20,65 @@ def test_readwriteconfig():
                     not hostmanagement_interface.clk
                 yield delay(5)
 
-        def clkWait(count=1):
+        def clkwait(count=1):
             while count:
                 yield hostmanagement_interface.clk.posedge
                 count -= 1
 
-        return write, hostclkdriver, managementInst
+        @instance
+        def testlogic():
+            yield clkwait(count=10)
+            yield hostmanagement_interface.writeconfig(0x239, 0x12345678)
+            yield clkwait(count=4)
+            yield hostmanagement_interface.readconfig(0x239)
+            assert hostmanagement_interface.rddata[32:] == intbv(0x12345678)
 
-    writeInst = writeconfig_operation()
-    writeInst.config_sim(trace=True)
-    writeInst.run_sim(duration=300)
+        return testlogic, hostclkdriver, managementInst
+
+    testInst = test()
+    testInst.config_sim(trace=False)
+    testInst.run_sim(duration=300)
+    testInst.quit_sim()
+
+
+def test_mdioclkgen():
+
+    @block
+    def test():
+        hostmanagement_interface = HostManagementInterface()
+        mdio_interface = MDIOInterface()
+        managementInst = management(hostmanagement_interface, mdio_interface)
+        print("Testing MDIO Clk Generator %s" % managementInst)
+
+        @instance
+        def hostclkdriver():
+            while True:
+                hostmanagement_interface.clk.next = \
+                    not hostmanagement_interface.clk
+                yield delay(5)
+
+        def clkwait(count=1):
+            while count:
+                yield hostmanagement_interface.clk.posedge
+                count -= 1
+
+        @instance
+        def testlogic():
+            yield clkwait(count=20)
+            yield hostmanagement_interface.writeconfig(0x340, 0x00000002)
+            yield mdio_interface.mdc.posedge
+            time_mdc = now()
+            yield mdio_interface.mdc.posedge
+            time_mdc = now() - time_mdc
+            yield hostmanagement_interface.clk.posedge
+            time_hostclk = now()
+            yield hostmanagement_interface.clk.posedge
+            time_hostclk = now() - time_hostclk
+            assert time_hostclk == time_mdc / 6
+
+        return testlogic, hostclkdriver, managementInst
+
+    testInst = test()
+    testInst.config_sim(trace=True)
+    testInst.run_sim(duration=300)
+    testInst.quit_sim
