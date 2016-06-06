ae181e04b4c81e11ca0aa0e81f53c7d0554daf84
Ravi Jain <ravijain056@gmail.com>
2016-06-06 20:30:51 +0530

    Implemented MDIO transactions.

--- gemac/management.py
+++ gemac/management.py
@@ -1,8 +1,18 @@
-from myhdl import block, Signal, intbv, always_seq
+from myhdl import block, Signal, intbv, always_seq, always_comb
 
 
-rx0, rx1, tx, flow, management, ucast0, \
+rx0, rx1, tx, flow, managementreg, ucast0, \
     ucast1, addrtable0, addrtable1, addrfiltermode = range(10)
+
+
+class mdioData:
+    def __init__(self, opcode, hostaddr, wrdata):
+        self.pre = intbv(0xFFFFFFFF)[32:]
+        self.st = intbv(0b01)[2:]
+        self.op = opcode[2:]
+        self.phyadd = hostaddr[10:5]
+        self.regadd = hostaddr[5:]
+        self.wrdata = wrdata[16:]
 
 
 @block
@@ -33,6 +43,11 @@ def management(host_interface, mdio_interface):
 
     """
 
+    mdioout = Signal(bool(0))
+    mdioin = Signal(bool(0))
+    mdiotri = Signal(bool(0))
+    mdio_interface.mdiodriver = mdio_interface.mdioio.driver()
+    hostclk_count = 0
     configregisters = [Signal(intbv(0))[32:0] for _ in range(10)]
 
     def getregisternumber(addr):
@@ -75,4 +90,59 @@ def management(host_interface, mdio_interface):
 
     # @TODO: Address Table Read.
 
-    return readConfig, writeConfig
+    @always_comb
+    def mdiodriver():
+        mdio_interface.mdiodriver.next = mdioout if mdiotri else None
+        mdioin.next = mdio_interface.mdioio
+
+    @always_seq(host_interface.clk.posedge, reset=None)
+    def mdcdriver():
+        clkDiv = (configregisters[managementreg][5:] + 1)  # * 2
+        hostclk_count += 1
+        if hostclk_count == clkDiv:
+            host_interface.mdc.next = not host_interface.mdc
+            hostclk_count = 0
+
+    # May req to recheck trigerring signal according to Page 87, Xilinx UG144
+    @always_seq(host_interface.clk.posedge, reset=None)
+    def mdioinitiate():
+        if host_interface.hostreq and host_interface.miimsel:
+            host_interface.miimrdy.next = 0
+            mdiodata = mdioData(host_interface.opcode,
+                                host_interface.regaddress,
+                                host_interface.wrdata)
+            yield host_interface.clk.posedge
+            if host_interface.opcode[1]:  # Read Operation
+                host_interface.rddata.next = mdioread(mdiodata)
+            elif not host_interface.opcode[1]:  # Write Operation
+                mdiowrite(mdiodata)
+            host_interface.miimrdy.next = 1
+
+    def mdiostream(datalist):
+        mdiotri.next = 1
+        yield mdio_interface.mdc.posedge
+        for data in datalist:
+            for i in reversed(range(len(data))):
+                mdioout.next = data[i]
+                yield mdio_interface.mdc.posedge
+        mdiotri.next = 0
+
+    def mdiowrite(mdiodata):
+        mdiodata.ta = intbv(0b10)[2:]
+        mdiostream([mdiodata.pre, mdiodata.st, mdiodata.op, mdiodata.phyadd,
+                    mdiodata.regadd, mdiodata.ta, mdiodata.wrdata])
+        host_interface.miimrdy.next = 1
+
+    def mdioread(mdiodata):
+        mdiostream([mdiodata.pre, mdiodata.st, mdiodata.op, mdiodata.phyadd,
+                    mdiodata.regadd])
+        yield mdio_interface.mdc.posedge
+        yield mdio_interface.mdc.posedge  # Two turnaround bits
+        host_interface.rddata.next = 0
+        rddata = intbv(0)[16:]
+        for i in reversed(range(len(rddata))):
+            yield mdio_interface.mdc.posedge
+            rddata[i] = mdioin
+        return rddata
+
+    return readConfig, writeConfig, mdiodriver, mdcdriver, mdioinitiate
