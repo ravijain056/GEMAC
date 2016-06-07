38e597e71c9080e1a0472200733af69d83810297
Ravi Jain <ravijain056@gmail.com>
2016-06-08 00:55:43 +0530

    Added Test for MDIO operations.

--- tests/test_mangement.py
+++ tests/test_mangement.py
@@ -79,6 +79,46 @@ def test_mdioclkgen():
         return testlogic, hostclkdriver, managementInst
 
     testInst = test()
-    testInst.config_sim(trace=True)
+    testInst.config_sim(trace=False)
     testInst.run_sim(duration=300)
-    testInst.quit_sim
+    testInst.quit_sim()
+
+
+def test_mdiooperation():
+
+    @block
+    def test():
+        hostmanagement_interface = HostManagementInterface()
+        mdio_interface = MDIOInterface()
+        managementInst = management(hostmanagement_interface, mdio_interface)
+        print("Testing MDIO Write Operation %s" % managementInst)
+
+        @instance
+        def hostclkdriver():
+            while True:
+                hostmanagement_interface.clk.next = \
+                    not hostmanagement_interface.clk
+                yield delay(5)
+
+        def clkwait(count=1):
+            while count:
+                yield hostmanagement_interface.clk.posedge
+                count -= 1
+
+        @instance
+        def testlogic():
+            yield clkwait(count=10)
+            yield hostmanagement_interface.writeconfig(0x340, 0x00000002)
+            yield mdio_interface.mdc.posedge
+            yield hostmanagement_interface.\
+                mdiowriteop(intbv(0b01), intbv(0b1001010100),
+                            intbv(0xA578), block=False)
+            yield clkwait(count=200)
+            assert True
+
+        return testlogic, hostclkdriver, managementInst
+
+    testInst = test()
+    testInst.config_sim(trace=False)
+    testInst.run_sim(duration=5000)
+    testInst.quit_sim()
